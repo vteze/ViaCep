0d6789de3a42c082c42847c0d89517d40a82b6ff
vteze <v.cruz001@edu.pucrs.br>
2021-08-12 15:36:43 -0300

    Sabes

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net.Http.Json;
+
+namespace DemoAsync2
+{
+    class Program
+    {
+        static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            // Console.WriteLine("Digite o seu CEP: ");
+            // var cep = Console.ReadLine();
+            // try
+            // {
+            //     string uri = $"https://viacep.com.br/ws/{cep}/json/";
+            //     var resposta = await client.GetAsync(uri);
+            //     Console.WriteLine(resposta.StatusCode);
+            //     Console.WriteLine(resposta.IsSuccessStatusCode);
+            //     Console.WriteLine(await resposta.Content.ReadAsStringAsync());
+            // }
+            // catch (Exception ex)
+            // {
+            //     Console.WriteLine(ex.Message);
+            // }
+
+            Console.WriteLine("Digite o seu CEP: ");
+            var cep = Console.ReadLine();
+            try 
+            {
+                string uri = $"https://viacep.com.br/ws/{cep}/json/";
+                var resposta = await client.GetFromJsonAsync<ViaCep>(uri);
+                Console.WriteLine(resposta.Localidade);
+                Console.WriteLine(resposta.Bairro);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ ViaCep.cs
@@ -0,0 +1,15 @@
+namespace DemoAsync2
+{
+    public class ViaCep
+    {
+        public string Cep { get; set; }
+        public string Lograudouro { get; set; }
+        public string Bairro { get; set; }
+        public string Localidade { get; set; }
+        public string Uf { get; set; }
+        public string Ibge { get; set; }
+        public string Gia { get; set; }
+        public string Ddd { get; set; }
+        public string Siafi { get; set; }
+    }
+}
